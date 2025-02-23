81b1ccfbec5c03d6e5e9b9ef4faff03cbde9ac13
abhay <abhayginoya962@gmail.com>
2025-02-23 13:11:24 +0530

    Remove extara code

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
                   Abhay Ginoya
-                </a>
                 . All Rights Reserved.
               </span>
             </center>
@@ -53,10 +51,8 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          © 2025{" "}
             Abhay Ginoya
-          </a>
           . All Rights Reserved.
         </span>
       </center>
@@ -68,12 +64,12 @@ function App() {
   const [showWelcome, setShowWelcome] = useState(true);
 
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
       </Routes>
-    </BrowserRouter>
+    // </BrowserRouter>
   );
 }
 
